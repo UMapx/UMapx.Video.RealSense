500021ae2b5baeb16d3f8fd8df652f89a491f4c9
Farkhat Ovezklychev <f.ovezklychev@smartmealservice.com>
2021-05-23 18:50:14 +0300

    Added summary and improve code readability

---
+++ RealSense.FaceID/MainWindow.xaml.cs
@@ -0,0 +1,242 @@
+﻿using Intel.RealSense;
+using Intel.RealSense.Math;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using UMapx.Core;
+using UMapx.Imaging;
+using FaceONNX;
+using FaceONNX.Core;
+
+namespace RealSense.FaceID
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Fields
+
+        private readonly IVideoSensorSource _realSenseVideoSource;
+        private readonly Grayscale _grayscale = Grayscale.BT709;
+        private readonly FaceDetectorLight _faceDetectorLight = new FaceDetectorLight();
+        private readonly Painter _painter = new Painter();
+        private static object _locker = new object();
+        private Bitmap _frame;
+        private Bitmap _depthFrame;
+        private Thread _procTask = null;
+        private System.Drawing.Rectangle _rectangle;
+
+        #endregion
+
+        #region Launcher
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _realSenseVideoSource = new RealSenseVideoSource();
+            _realSenseVideoSource.NewSensorsFrames += _realSenseVideoSource_NewSensorsFrames;
+            _realSenseVideoSource.Start();
+        }
+
+        #endregion
+
+        #region Processing Properties
+
+        /// <summary>
+        /// Get Color Bitmap and dispose previous
+        /// </summary>
+        Bitmap ColorBitmap
+        {
+            get
+            {
+                if (_frame is null)
+                    return null;
+
+                Bitmap frame;
+
+                lock (_locker)
+                {
+                    frame = (Bitmap)_frame.Clone();
+                }
+
+                return frame;
+            }
+            set
+            {
+                lock (_locker)
+                {
+                    if (_frame is object)
+                    {
+                        _frame.Dispose();
+                        _frame = null;
+                    }
+
+                    _frame = value;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Get Depth Bitmap and dispose previous
+        /// </summary>
+        Bitmap DepthBitmap
+        {
+            get
+            {
+                if (_depthFrame is null)
+                    return null;
+
+                Bitmap depthFrame;
+
+                lock (_locker)
+                {
+                    depthFrame = (Bitmap)_depthFrame.Clone();
+                }
+
+                return depthFrame;
+            }
+            set
+            {
+                lock (_locker)
+                {
+                    if (_depthFrame is object)
+                    {
+                        _depthFrame.Dispose();
+                        _depthFrame = null;
+                    }
+
+                    _depthFrame = value;
+                }
+            }
+        }
+
+        public System.Drawing.Rectangle Rectangle
+        {
+            get
+            {
+                lock (_locker)
+                    return _rectangle;
+            }
+            set
+            {
+                lock (_locker)
+                    _rectangle = value;
+            }
+        }
+
+        #endregion
+
+        #region Frame Handling Methods
+
+        /// <summary>
+        /// Frame handling on event call.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="eventArgs"></param>
+        private void _realSenseVideoSource_NewSensorsFrames(object sender, NewSensorsEventArgs eventArgs)
+        {
+            var frames = eventArgs.Frames;
+            var tempColor = (Bitmap)frames[0].Clone();
+            var tempDepth = (Bitmap)frames[1].Clone();
+
+            ColorBitmap = tempColor;
+            _grayscale.Apply(tempDepth);
+            DepthBitmap = tempDepth;
+
+            InvokeDrawing();
+
+            // do job
+            if (_procTask == null)
+            {
+                // main process
+                _procTask = new Thread(() => ProcessFrame(ColorBitmap))
+                {
+                    IsBackground = true,
+                    Priority = ThreadPriority.Lowest
+                };
+                _procTask.Start();
+            }
+        }
+
+        /// <summary>
+        /// Frame processing for face recognition
+        /// </summary>
+        /// <param name="imageFrame"></param>
+        private void ProcessFrame(Bitmap imageFrame)
+        {
+            // color
+            Rectangle = _faceDetectorLight.Forward(imageFrame).FirstOrDefault();
+
+            // drawing face rect if the face area is not empty (?)
+            if (!Rectangle.IsEmpty)
+            {
+                InvokeDrawing();
+            }
+
+            // dispose
+            imageFrame.Dispose();
+            _procTask = null;
+        }
+
+        /// <summary>
+        /// Draw calculated <see cref="BitmapImage"/> based on <see cref="RealSenseVideoSource"/> bitmap converted frames
+        /// in <see cref="Window"/> Image element
+        /// </summary>
+        private void InvokeDrawing()
+        {
+            try
+            {
+                var paintData = new PaintData()
+                {
+                    Rectangle = this.Rectangle,
+                    Title = string.Empty
+                };
+
+                // color drawing
+                var printColor = ColorBitmap;
+
+                if (printColor is object)
+                {
+                    lock (_locker) _painter.Draw(printColor, paintData);
+
+                    var bitmapColor = Helpers.ToBitmapImage(printColor);
+                    bitmapColor.Freeze();
+                    Dispatcher.BeginInvoke(new ThreadStart(delegate { imgColor.Source = bitmapColor; }));
+                }
+
+                // depth drawing
+                var printDepth = DepthBitmap;
+
+                if (printDepth is object)
+                {
+                    lock (_locker) _painter.Draw(printDepth, paintData);
+
+                    var bitmapDepth = Helpers.ToBitmapImage(printDepth);
+                    bitmapDepth.Freeze();
+                    Dispatcher.BeginInvoke(new ThreadStart(delegate { imgDepth.Source = bitmapDepth; }));
+                }
+            }
+            catch { }   // skip corrupted or empty frame
+        }
+
+        #endregion
+    }
+}
---
+++ RealSense.FaceID/RealSenseVideoSource.cs
@@ -0,0 +1,279 @@
+﻿using Intel.RealSense;
+using System;
+using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using UMapx.Video;
+using static RealSense.FaceID.SensorEvents;
+
+namespace RealSense.FaceID
+{
+    public class RealSenseVideoSource : IVideoSensorSource, IDisposable
+    {
+        #region Fields
+
+        private readonly Colorizer _colorizer;
+        private readonly Pipeline _pipeline;
+        private readonly Context _ctx;
+        private readonly Config _cfg;
+        private CancellationTokenSource _tokenSource;
+        private Bitmap _colorBitmap;
+        private Bitmap _colorizedDepthBitmap;
+        private int _framesReceived;
+        private long _bytesReceived;
+
+        #endregion
+
+        #region Constructor
+
+        /// <summary>
+        /// Launch single Intel RealSense Camera with default <see cref="Stream.Color"/> to <see cref="Stream.Depth"/>
+        /// </summary>
+        public RealSenseVideoSource()
+        {
+            _colorizer = new Colorizer();
+
+            _pipeline = new Pipeline();
+
+            _ctx = new Context();
+
+            var devices = _ctx.QueryDevices();
+            var dev = devices.FirstOrDefault();
+
+            Source = dev.Info[CameraInfo.Name];
+            SerialNumber = dev.Info[CameraInfo.SerialNumber];
+            FirmwareVersion = dev.Info[CameraInfo.FirmwareVersion];
+
+            var sensors = dev.Sensors;
+
+            var depthSensor = sensors[0];
+            var depthProfile = depthSensor.StreamProfiles
+                                .Where(p => p.Stream == Stream.Depth)
+                                .OrderBy(p => p.Framerate)
+                                .Select(p => p.As<VideoStreamProfile>()).First();
+
+
+            var colorSensor = sensors[1];
+            var colorProfile = colorSensor.StreamProfiles
+                                .Where(p => p.Stream == Stream.Color)
+                                .OrderBy(p => p.Framerate)
+                                .Select(p => p.As<VideoStreamProfile>()).First();
+
+            _cfg = new Config();
+
+            _cfg.EnableStream(Stream.Depth, depthProfile.Width, depthProfile.Height, depthProfile.Format, depthProfile.Framerate);
+            _cfg.EnableStream(Stream.Color, colorProfile.Width, colorProfile.Height, colorProfile.Format, colorProfile.Framerate);
+
+            _tokenSource = new CancellationTokenSource();
+        }
+
+        // later create constructor with cutom config based on json file
+
+        #endregion
+
+        #region Methods
+
+        public string Source { get; private set; }
+
+        /// <summary>
+        /// Serial number of Intel RealSense camera.
+        /// </summary>
+        public string SerialNumber { get; private set; }
+
+        /// <summary>
+        /// Firmware version of Intel RealSense camera.
+        /// </summary>
+        public string FirmwareVersion { get; private set; }
+
+        /// <summary>
+        /// Received frames count.
+        /// </summary>
+        /// 
+        /// <remarks>Number of frames the video source provided from the moment of the last
+        /// access to the property.
+        /// </remarks>
+        /// 
+        public int FramesReceived
+        {
+            get
+            {
+                int frames = _framesReceived;
+                _framesReceived = 0;
+                return frames;
+            }
+        }
+
+        /// <summary>
+        /// Received bytes count.
+        /// </summary>
+        /// 
+        /// <remarks>Number of bytes the video source provided from the moment of the last
+        /// access to the property.
+        /// </remarks>
+        /// 
+        public long BytesReceived
+        {
+            get
+            {
+                long bytes = _bytesReceived;
+                _bytesReceived = 0;
+                return bytes;
+            }
+        }
+
+        /// <summary>
+        /// State of the video source.
+        /// </summary>
+        /// 
+        /// <remarks>Current state of video source object - running or not.</remarks>
+        /// 
+        public bool IsRunning { get; private set;}
+
+        /// <summary>
+        /// Intel RealSense frames actions event handler. 
+        /// </summary>
+        public event NewSensorsEventHandler NewSensorsFrames;
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public event NewFrameEventHandler NewFrame;
+
+        public event VideoSourceErrorEventHandler VideoSourceError;
+
+        public event PlayingFinishedEventHandler PlayingFinished;
+
+        /// <summary>
+        /// Start video source.
+        /// </summary>
+        /// 
+        /// <remarks>Starts video source and return execution to caller. Video source
+        /// object creates background thread and notifies about new frames with the
+        /// help of <see cref="NewFrame"/> event.</remarks>
+        /// 
+        public void Start()
+        {
+            try
+            {
+                IsRunning = true;
+                _framesReceived = 0;
+                _bytesReceived = 0;
+                var pipelineProfile = _pipeline.Start(_cfg);
+
+                Task.Factory.StartNew(() =>
+                {
+                    while (!_tokenSource.Token.IsCancellationRequested)
+                    {
+                        using (var frameset = _pipeline.WaitForFrames())
+                        {
+                            using var colorFrame = frameset.ColorFrame;
+                            using var depthFrame = frameset.DepthFrame;
+
+                            using var colorizedDepth = _colorizer.Process<VideoFrame>(depthFrame);
+
+                            _colorBitmap?.Dispose();
+                            _colorizedDepthBitmap?.Dispose();
+
+                            _colorBitmap = Helpers.ToBitmap(colorFrame, PixelFormats.Rgb24);
+                            _colorizedDepthBitmap = Helpers.ToBitmap(colorizedDepth, PixelFormats.Rgb24);
+
+                            OnNewFrame(_colorBitmap, _colorizedDepthBitmap);
+                        }
+                    }
+                }, _tokenSource.Token);
+            }
+            catch (Exception ex)
+            {
+                VideoSourceError?.Invoke(this, new VideoSourceErrorEventArgs(ex.Message));
+                IsRunning = false;
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Stop video source.
+        /// </summary>
+        /// 
+        /// <remarks><para>Stops video source aborting its thread.</para>
+        /// 
+        /// <para><note>Since the method aborts background thread, its usage is highly not preferred
+        /// and should be done only if there are no other options. The correct way of stopping camera
+        /// is <see cref="SignalToStop">signaling it stop</see> and then
+        /// <see cref="WaitForStop">waiting</see> for background thread's completion.</note></para>
+        /// </remarks>
+        /// 
+        public void Stop()
+        {
+            _tokenSource.Cancel();
+            _tokenSource.Dispose();
+            _tokenSource = new CancellationTokenSource();
+            PlayingFinished?.Invoke(this, ReasonToFinishPlaying.StoppedByUser);
+            IsRunning = false;
+        }
+
+        /// <summary>
+        /// Signal video source to stop its work.
+        /// </summary>
+        /// 
+        /// <remarks>Signals video source to stop its background thread, stop to
+        /// provide new frames and free resources.</remarks>
+        /// 
+        public void SignalToStop()
+        {
+            Stop();
+        }
+
+        /// <summary>
+        /// Wait for video source has stopped.
+        /// </summary>
+        /// 
+        /// <remarks>Waits for source stopping after it was signalled to stop using
+        /// <see cref="SignalToStop"/> method.</remarks>
+        /// 
+        public void WaitForStop()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region Private voids
+        /// <summary>
+        /// Called when video source gets new frame
+        /// </summary>
+        /// <param name="colorBitmap"></param>
+        /// <param name="colorizedDepthBitmap"></param>
+        private void OnNewFrame(Bitmap colorBitmap, Bitmap colorizedDepthBitmap)
+        {
+            _framesReceived++;
+            _bytesReceived += 
+                colorBitmap.Width * colorBitmap.Height * (Bitmap.GetPixelFormatSize(colorBitmap.PixelFormat) >> 3) +
+                colorizedDepthBitmap.Width * colorizedDepthBitmap.Height * (Bitmap.GetPixelFormatSize(colorizedDepthBitmap.PixelFormat) >> 3);
+
+            // NewFrame?.Invoke(this, new NewFrameEventArgs(colorBitmap));      // delete later
+            NewSensorsFrames?.Invoke(this, new NewSensorsEventArgs(colorBitmap, colorizedDepthBitmap));
+        }
+
+        #endregion
+
+        #region IDisposable
+
+        /// <summary>
+        /// Recycles objects necessary for disposal
+        /// </summary>
+        public void Dispose()
+        {
+            _colorizer?.Dispose();
+            _pipeline?.Dispose();
+            _ctx?.Dispose();
+            _cfg?.Dispose();
+            _tokenSource?.Dispose();
+            _colorBitmap?.Dispose();
+            _colorizedDepthBitmap?.Dispose();
+        }
+
+        #endregion
+    }
+}
